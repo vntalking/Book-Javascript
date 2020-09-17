9e7af5a80f95af5e2d33594b6bd484a4b85cf3d7
vntalking <anhson.duong@outlook.com>
2020-09-17 21:53:02 +0700

    chap 3 - add global variable

--- chap3/variable-scope/src/index.js
+++ chap3/variable-scope/src/index.js
@@ -1,2 +1,8 @@
-let message = "Xin chào độc giả quý mến";
-alert(message);
+var x = 10; //Đây là global variable bởi vì nó không khái báo trong hàm
+var turnToZero = function (number) {
+  number = 0;
+  console.log("Giá trị của biến x là: " + x);
+};
+
+turnToZero(x);
+console.log("Bên ngoài hàm, giá trị của biến x là: " + x);
