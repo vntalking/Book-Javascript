03d5ca21d88f4fa9e634582b2459bed565bfa49f
vntalking <anhson.duong@outlook.com>
2020-09-17 22:17:56 +0700

    how to modify value of globle variable

--- chap3/variable-scope/src/index.js
+++ chap3/variable-scope/src/index.js
@@ -1,8 +1,17 @@
-var x = 10; //Đây là global variable bởi vì nó không khái báo trong hàm
-var turnToZero = function (number) {
-  number = 0;
-  console.log("Giá trị của biến x là: " + x);
-};
+// var x = 10; //Đây là global variable bởi vì nó không khái báo trong hàm
+// var turnToZero = function (number) {
+//   number = 0;
+//   console.log("Giá trị của biến x là: " + x);
+// };
 
-turnToZero(x);
-console.log("Bên ngoài hàm, giá trị của biến x là: " + x);
+// turnToZero(x);
+// console.log("Bên ngoài hàm, giá trị của biến x là: " + x);
+
+var x = 10; // Đây là global variable bởi vì nó không khái báo trong hàm
+var turnToZero = function () {
+  x = 0;
+  console.log("Giá trị của biến x hiện tại là: " + x);
+};
+console.log("Trước khi gọi hàm, giá trị của biến x là: " + x);
+turnToZero();
+console.log("Sau khi gọi hàm, giá trị của biến x là: " + x);
